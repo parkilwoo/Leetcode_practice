7091d53dcded67eb20adb8ad876249e5918513ce
PIW <56834479+parkilwoo@users.noreply.github.com>
2025-09-14 00:24:19 +0900

    Time: 0 ms (100%), Space: 18.7 MB (61.92%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,18 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        memoization = {}
-        for i in range(len(nums)):
-            num_v = nums[i]
-            minus_val = target - num_v
-            if memoization.get(minus_val) is not None:
-                return [memoization.get(minus_val), i]
-            memoization[num_v] = i
+        """
+        배열 nums가 있을 때, 2개를 선택하여 target에 일치하는 인덱스를 return
+        정확하게 1개의 답이 있음
+
+        memoization을 이용해서 현재 선택된 값과 target의 차이가 memo에 있는지 체크
+        pre+cur=k
+        pre=k-cur
+        """
+        
+        v_idx_map = {}
+
+        for i, num in enumerate(nums):
+            minus = target-num
+            if minus in v_idx_map:
+                return [v_idx_map[minus], i]
+            v_idx_map[num] = i
