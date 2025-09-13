57c77441f03b3ace62e1ec7e42671f8b49c03688
PIW <56834479+parkilwoo@users.noreply.github.com>
2025-09-13 23:43:49 +0900

    Time: 19 ms (36.47%), Space: 22.6 MB (25.01%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,14 +1,20 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        if not strs:
-            return [[""]]
-
-        string_hash = {}
-        for string in strs:
-            order_string = "".join(sorted(string))
-            if string_hash.get(order_string):
-                string_hash.get(order_string).append(string)
-                continue
-            string_hash[order_string] = [string]
+        """
+        문자열 배열 strs가 주어졌을 때, 아나그램끼리 리스트로 묶어서 return
+        아나그램은 특정 문자의 순열이면 됨
+        """
         
-        return list(string_hash.values())
+        hash_map = {}
+        for s in strs:
+            ord_list = [0] * 26
+            for ch in s:
+                ord_list[ord(ch) - ord('a')] += 1
+            
+            key = tuple(ord_list)
+            if key in hash_map:
+                hash_map[key].append(s)
+            else:
+                hash_map[key] = [s]
+
+        return list(hash_map.values())
