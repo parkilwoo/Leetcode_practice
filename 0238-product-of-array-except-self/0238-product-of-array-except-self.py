b9183c9edf71a76bfd31756f72585e187976f8d6
PIW <56834479+parkilwoo@users.noreply.github.com>
2025-09-14 01:04:21 +0900

    Time: 19 ms (86.2%), Space: 23.3 MB (79.97%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,15 +1,31 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        nums_len = len(nums)
-        result = [1] * nums_len
+        """
+        정수배열 nums가 주어졌을 때, 
+        answer를 리턴해라
+        answer[i] = nums[i]를 제외한 nums모든 요소들의 곱
 
-        prefix = 1
-        for i in range(nums_len):
-            result[i] = prefix
-            prefix *= nums[i]
+        어떤 위치 a에서 자신을 제외한 곱들의 값은?
+        -> a의 왼쪽 곱 * a의 오른쪽 
+        
+        """
+        
+        n = len(nums)
+        result = [1] * n
 
-        postfix = 1
-        for i in range(nums_len-1, -1, -1):
-            result[i] *= postfix
-            postfix *= nums[i]
+        # 본인을 제외한 왼쪽 누적곱 적용
+        for i in range(1, n):
+            result[i] = result[i-1] * nums[i-1]
+        
+        # 본인을 제외한 오른쪽 누적 곱
+        prefix_right = 1
+        for i in range(n-1, -1, -1):
+            result[i] *= prefix_right
+            prefix_right *= nums[i]
+
         return result
+
+        
+        
+        
+            
