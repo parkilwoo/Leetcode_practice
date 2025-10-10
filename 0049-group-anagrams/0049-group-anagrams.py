0361cc331ff08bc3e02e4836b80ea484a52053a8
PIW <56834479+parkilwoo@users.noreply.github.com>
2025-10-10 21:22:23 +0900

    Time: 23 ms (18.68%), Space: 22.8 MB (11.83%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,20 +1,22 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         """
-        문자열 배열 strs가 주어졌을 때, 아나그램끼리 리스트로 묶어서 return
-        아나그램은 특정 문자의 순열이면 됨
+        문자열배열 strs가 주어졌을 때, anagram끼리 배열로 묶어서 리턴
+        배열의 순서는 상관 없음
+        Anagram 판단하는 방법
         """
-        
-        hash_map = {}
-        for s in strs:
-            ord_list = [0] * 26
-            for ch in s:
-                ord_list[ord(ch) - ord('a')] += 1
-            
-            key = tuple(ord_list)
-            if key in hash_map:
-                hash_map[key].append(s)
-            else:
-                hash_map[key] = [s]
+        memoization = dict()
+        group_idx = 0
+        for string in strs:
+            count = [0] * 26
+            for s in string:
+                count[ord(s) - ord('a')] += 1
+            tuple_cnt = tuple(count)
+            if tuple_cnt not in memoization:
+                memoization[tuple_cnt] = list()
+            memoization[tuple_cnt].append(string)
 
-        return list(hash_map.values())
+        return list(memoization.values())
+
+        
+                
