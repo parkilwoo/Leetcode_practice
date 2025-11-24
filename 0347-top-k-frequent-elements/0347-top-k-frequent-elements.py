d731ee0fc74714b9d7d0369daa2d8c0befb21248
PIW <56834479+parkilwoo@users.noreply.github.com>
2025-11-24 22:28:36 +0900

    Time: 7 ms (50.39%), Space: 21.3 MB (61.06%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,18 +1,34 @@
 from collections import Counter
+import heapq
+
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         # 가장 많이 나온 갯수 뽑기
         num_counter = Counter(nums)
 
         # 1. sorting 방식
-        sorting = []
-        for num, cnt in num_counter.items():
-            sorting.append((cnt, num))
+        # sorting = []
+        # for num, cnt in num_counter.items():
+        #     sorting.append((cnt, num))
         
-        sorting.sort()
+        # sorting.sort()
+        # res = []
+        # while k > 0:
+        #     res.append(sorting.pop()[1])
+        #     k -= 1
+
+        # return res
+
+        # 2. maxHeap 방식
+        heap = []
+
+        for num, cnt in num_counter.items():
+            heapq.heappush(heap, (-cnt, num))
+
         res = []
         while k > 0:
-            res.append(sorting.pop()[1])
+            _, num = heapq.heappop(heap)
+            res.append(num)
             k -= 1
-
+        
         return res
