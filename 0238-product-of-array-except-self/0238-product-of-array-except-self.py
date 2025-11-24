e5f84feebd715c537daf520ecef15937698f1300
PIW <56834479+parkilwoo@users.noreply.github.com>
2025-11-24 22:07:32 +0900

    Time: 37 ms (19.46%), Space: 27.6 MB (5.72%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,34 +1,32 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        """
-        정수배열 nums가 주어졌을 때, 
-        answer를 리턴해라
-        answer[i] = nums[i]를 제외한 nums모든 요소들의 곱
-
-        어떤 위치 a에서 자신을 제외한 곱들의 값은?
-        -> a의 왼쪽 곱 * a의 오른쪽 
-
-        1. nums를 반복하면서 현재 자신을 제외한 왼쪽 곱들의 값을 기록한다
-        2. nums를 오른쪽부터 반복하면서 자신을 제외한 오른쪽 곱들의 값을 기록한다
-        3. 1번과 2번에서 구한 값들의 곱을 리턴
-        """
-        
+        # 1. brute force
         n = len(nums)
-        result = [1] * n
+        res = [1] * n
+        # for i in range(n):
+        #     prod = 1
+        #     for j in range(n):
+        #         if i == j:
+        #             continue
+        #         prod *= nums[j]
+        #     res[i] = prod
+        # return res
 
-        # 본인을 제외한 왼쪽 누적곱 적용
+        # 나를 제외한 곱의 값: 나의 왼쪽 곱 * 나의 오른쪽 곱
+        # f(i) = left(i) * right(i)
+        # left(i) = nums[0] * num[1] * ...nums[i-1]
+        # right(i) = nums[i+1] * nums[i+2] * ... num[n-1]
+        left = [1] * n
+
         for i in range(1, n):
-            result[i] = result[i-1] * nums[i-1]
+            left[i] = left[i-1] * nums[i-1]
         
-        # 본인을 제외한 오른쪽 누적 곱
-        prefix_right = 1
-        for i in range(n-1, -1, -1):
-            result[i] *= prefix_right
-            prefix_right *= nums[i]
+        right = [1] * n
+        for i in range(n-2, -1, -1):
+            right[i] = right[i+1] * nums[i+1]
 
-        return result
+        res = [0] * n
+        for i in range(n):
+            res[i] = left[i] * right[i]
 
-        
-        
-        
-            
+        return res
