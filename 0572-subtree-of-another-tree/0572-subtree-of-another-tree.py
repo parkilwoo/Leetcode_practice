f1095b9898e77a473545035e7924c20bbb8a3600
PIW <56834479+parkilwoo@users.noreply.github.com>
2025-12-05 20:11:54 +0900

    Time: 39 ms (82.36%), Space: 18 MB (59.41%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
@@ -4,27 +4,23 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
+from collections import deque
 class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        # 1. Base Case: root가 없으면 비교 자체가 불가능하므로 False
         if not root:
             return False
-        
-        # 2. 현재 root에서 시작하는 트리가 subRoot와 같은지 확인
+
         if self.isSameTree(root, subRoot):
             return True
         
-        # 3. 아니라면, 왼쪽 자식이나 오른쪽 자식에서 다시 탐색 (재귀)
         return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
 
-    # 두 트리가 완전히 같은지 확인하는 헬퍼 함수
     def isSameTree(self, p, q):
-        # 둘 다 None이면 같음
+        # 둘다 None일 경우
         if not p and not q:
             return True
-        # 둘 중 하나만 None이거나, 값이 다르면 다름
-        if not p or not q or p.val != q.val:
+        # 하나만 None이거나 val가 다를경우
+        if not(p and q) or p.val != q.val:
             return False
-        # 왼쪽과 오른쪽 자식들도 모두 같아야 함
+
         return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
